15ca981f61cdc9f534a50cffab62e9f8d2ac6e01
Omar <omarh.ismail1@gmail.com>
2019-05-11 18:31:50 +0200

    Mostly finished Login Screen UI

--- app/src/main/java/com/m/warhit/todoapp/LoginActivity.java
+++ app/src/main/java/com/m/warhit/todoapp/LoginActivity.java
@@ -2,12 +2,29 @@ package com.m.warhit.todoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText ET_username;
+    Button login_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ET_username = findViewById(R.id.username);
+        login_button = findViewById(R.id.login_button);
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Hi there", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
